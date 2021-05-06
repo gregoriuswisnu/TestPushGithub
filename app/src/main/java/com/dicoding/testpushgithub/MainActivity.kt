5d7c444d338781ba1ebfd0773117727c9a8e132a
gregoriuswisnu <wisnu.gregorius1@gmail.com>
2021-05-06 18:45:04 +0700

    Update 2

--- app/src/main/java/com/dicoding/testpushgithub/MainActivity.kt
+++ app/src/main/java/com/dicoding/testpushgithub/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    val res: String = ""
+    val res: String = "Sat"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
